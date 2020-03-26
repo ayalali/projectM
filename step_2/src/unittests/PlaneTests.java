2e7ddbf04cd127667d51f169fc211281bc674bb9
ayala <ayala@DESKTOP-FM0ES7E>
2020-03-26 20:44:24 +0200

    second commit

---
+++ step_2/src/unittests/PlaneTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ayala
+ *
+ */
+public class PlaneTests {
+
+	/**
+	 * Test method for {@link geometries.Plane#getNormal(primitives.Point3D)}.
+	 */
+	@Test
+	public void testGetNormalPoint3D() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ step_2/src/unittests/PolygonTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ayala
+ *
+ */
+public class PolygonTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
