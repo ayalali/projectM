bd4ffd03b5779684fc48d082e25f62b80e05e23b
ayala <ayala@DESKTOP-FM0ES7E>
2020-03-26 20:49:49 +0200

    third commit

---
+++ step_2/src/unittests/TriangleTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ayala
+ *
+ */
+public class TriangleTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ step_2/src/unittests/TubeTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ayala
+ *
+ */
+public class TubeTests {
+
+	/**
+	 * Test method for {@link geometries.Tube#getNormal(primitives.Point3D)}.
+	 */
+	@Test
+	public void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
