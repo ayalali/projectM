81ffe2b13ae952de18c56bd06ed5e64e23de098a
ayala <ayala@DESKTOP-FM0ES7E>
2020-03-28 22:15:55 +0300

    TriangleTests + PlaneTests done

--- step_2/src/unittests/PlaneTests.java
+++ step_2/src/unittests/PlaneTests.java
@@ -4,8 +4,12 @@
 package unittests;
 
 import static org.junit.Assert.*;
+import geometries.Plane;
+import primitives.Point3D;
+import primitives.Vector;
 
 import org.junit.Test;
+import static primitives.Util.*;
 
 /**
  * Unit tests for geometries.Plane class
@@ -20,7 +24,12 @@ public class PlaneTests {
 	 */
 	@Test
 	public void testGetNormalPoint3D() {
-		fail("Not yet implemented");
+		
+		// ============ Equivalence Partitions Tests ==============
+		
+		Plane plane = new Plane(new Point3D(1,2,3), new Vector(new Point3D(1, 3, 0)));
+		assertEquals("ERROR: Plane.getNormal() wrong value", new Vector(new Point3D(1, 3, 0)).normalize(), plane.getNormal(new Point3D(1,2,3)).normalize());
+		
 	}
 
 }
--- step_2/src/unittests/TriangleTests.java
+++ step_2/src/unittests/TriangleTests.java
@@ -4,6 +4,10 @@
 package unittests;
 
 import static org.junit.Assert.*;
+import geometries.Triangle;
+import primitives.Point3D;
+import primitives.Util;
+import primitives.Vector;
 
 import org.junit.Test;
 
@@ -17,7 +21,18 @@ public class TriangleTests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		// ============ Equivalence Partitions Tests ==============
+		
+		Point3D p1 = new Point3D(1, 2, 1);
+		Point3D p2 = new Point3D(-3, 0, 5);
+		Point3D p3 = new Point3D(2, -2, -7);
+		Vector v1 = new Vector(p2.subtract(p1)); //p1->p2
+		Vector v2 = new Vector(p3.subtract(p1)); //p1->p3
+		Triangle triangle = new Triangle(p1, p2, p3);
+		Vector v3 = new Vector(v1.crossProduct(v2)); //expected
+		assertEquals("ERROR: Triangle.getNormal() wrong value", v3, triangle.getNormal(p1));
+		
 	}
 
 }
