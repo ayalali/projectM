a25f10553a57558a728061440a4b564498e54509
ayala <ayala@DESKTOP-FM0ES7E>
2020-04-12 14:07:36 +0300

    just started

--- step_3/src/geometries/Sphere.java
+++ step_3/src/geometries/Sphere.java
@@ -1,8 +1,11 @@
 package geometries;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import primitives.*;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 
 /**
  * Sphere represents a 3D sphere, that is a redial geometry.
@@ -11,7 +14,7 @@ import primitives.*;
  *
  */
 public class Sphere extends RadialGeometry{
-	////
+	
 	/**
 	 * the center of the sphere by 3D Cartesian coordinate.
 	 */
@@ -50,10 +53,33 @@ public class Sphere extends RadialGeometry{
 		return new Vector(p.subtract(this._center)).normalize();
 	}
 
+	
 	@Override
 	public List<Point3D> findIntersections(Ray r) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Point3D> lst = new ArrayList<Point3D>();
+		Vector L = new Vector(_center);
+		Vector helpVector = new Vector(r.get_direction());			
+		helpVector.normalize();
+		double tm = L.dotProduct(helpVector);
+		double d = Math.sqrt(Math.pow(L.length(), 2) - Math.pow(tm, 2));
+		if (d > get_radius()) 
+		{
+			return lst;
+		}
+		double th = Math.sqrt(Math.pow(get_radius(), 2) - Math.pow(d, 2));
+		double t1 = tm - th;
+		double t2 = tm + th;
+		Vector help1Vector = new Vector(helpVector);
+		help1Vector.scale(t1);
+		lst.add(new Point3D(help1Vector.get_head()));
+		if (d == get_radius()) 
+		{
+			return lst;
+		}
+		Vector help2Vector = new Vector(helpVector);
+		help2Vector.scale(t2);
+		lst.add(help2Vector.get_head());
+		return lst;
 	}
 
 
