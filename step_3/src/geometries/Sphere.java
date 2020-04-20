307d502ece6ef700016737a77ea16ec0d82c710a
ayala <ayala@DESKTOP-FM0ES7E>
2020-04-20 14:13:18 +0300

    Sphere intersections + test done

--- step_3/src/geometries/Sphere.java
+++ step_3/src/geometries/Sphere.java
@@ -55,37 +55,84 @@ public class Sphere extends RadialGeometry{
 
 	
 	/**
+	 * @param r ray sent toward sphere
+	 * @return o-2 points intersections
+	 * 
+	 * P0 = ray's point
+	 * 
+	 * L = length from ray's beginning to sphere's center = center - ray.head
+	 * 
+	 * V = vector from ray's point to sphere's center = _center.subtract(P)
+	 * 
+	 * tm = length of the ray to the point perpendicular to the sphere's center
+	 *	  = L * W = L.dotproduct(V)
+	 * 
+	 * D = perpendicular length from tm to sphere's center
+	 *	 = sqrt(L.length^2 - tm^2)
 	 *
+	 * if D > sphere's radius there is no intersections.
+	 * if D = radius:
+	 * there is two intersections.
+	 * 
+	 * th = length from first point on sphere to radius
+	 * 	  = sqrt(radius^2 - D^2)
 	 *
+	 * t1 = tm - th
+	 * t2 = tm + th
+	 * 
+	 * P1 = ray.head + t1 * V
+	 * P2 = ray.head + t2 * V
 	 *
-	 *
+	 * if D == radius:
+	 * there is only one intersection
+	 * 
+	 * P1 = ray.head + t1 * V
 	 *
 	 */
 	@Override
-	public List<Point3D> findIntersections(Ray r) {
+	public ArrayList<Point3D> findIntersections(Ray r) {
 		ArrayList<Point3D> lst = new ArrayList<Point3D>();
-		Vector L = new Vector(_center);
-		Vector helpVector = new Vector(r.get_direction());			
-		helpVector.normalize();
-		double tm = L.dotProduct(helpVector);
-		double d = Math.sqrt(Math.pow(L.length(), 2) - Math.pow(tm, 2));
-		if (d > get_radius()) 
+		Point3D P0 = r.get_point();
+		Vector V = new Vector(r.get_direction());			
+		V.normalize();
+		Vector L;
+		try 
 		{
+			L = new Vector(_center.subtract(P0));
+		} 
+		catch (Exception e) 
+		{
+			lst.add(P0.add(V.scale(get_radius())));
 			return lst;
 		}
-		double th = Math.sqrt(Math.pow(get_radius(), 2) - Math.pow(d, 2));
+		double tm = V.dotProduct(L);
+		double D = Math.sqrt(L.length()*L.length() - tm*tm);
+		if (D > get_radius())
+		{
+			return null;
+		}
+		double thSquared = get_radius()*get_radius() - D*D;
+		double th;
+		if (thSquared <= 0)
+			return null;
+		th = Math.sqrt(thSquared);
 		double t1 = tm - th;
 		double t2 = tm + th;
-		Vector help1Vector = new Vector(helpVector);
-		help1Vector.scale(t1);
-		lst.add(new Point3D(help1Vector.get_head()));
-		if (d == get_radius()) 
+		if (t1 > 0)
 		{
-			return lst;
+			Point3D P1 = new Point3D(r.get_point().add(V.scale(t1)));
+			lst.add(P1);
 		}
-		Vector help2Vector = new Vector(helpVector);
-		help2Vector.scale(t2);
-		lst.add(help2Vector.get_head());
+		if(D == get_radius())
+			return lst;
+		if (t2 > 0)
+		{
+			Point3D P2 = new Point3D(r.get_point().add(V.scale(t2)));
+			lst.add(P2);
+		}
+		if (lst.isEmpty())
+			return null;
+		
 		return lst;
 	}
 
