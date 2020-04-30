b1d97234231f8702e8ff8df56ccd06056919ac53
נעמה <@DESKTOP-AMCHM4M>
2020-04-30 14:54:19 +0300

    javaDoc not finished!!

--- step_5/src/elements/AmbientLight.java
+++ step_5/src/elements/AmbientLight.java
@@ -2,15 +2,34 @@ package elements;
 
 import primitives.Color;
 
+/**
+ * Ambient light is a  ambient light source represents a fixed-intensity and 
+ * fixed-color light source that affects all objects in the scene equally.
+ * 
+ * @author ayala and naama
+ *
+ */
 public class AmbientLight {
 
+	/**
+	 * _intensity (Ip) 
+	 */
 	private Color _intensity;
 
+	/**
+	 * @param _intensity (Ip) the color we need to considered of the light in the scene
+	 */
 	public AmbientLight(Color _intensity) {
 		this._intensity = _intensity;
 	}
+	/**
+	 * @param _intensity (Ia) the color we need to considered of the light in the scene
+	 * @param _ka (Ka) 
+	 * 
+	 * Ip = Ia * Ka
+	 */
 	public AmbientLight(Color _intensity, double _ka) {
-		this._intensity = _intensity;
+		this._intensity = _intensity.scale(_ka);
 	}
 	
 	public Color get_intensity() {
