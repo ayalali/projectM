906db794f97e66ac35e3ef9ab66d04d573562708
ayala <ayala@DESKTOP-FM0ES7E>
2020-05-07 13:04:22 +0300

    from moodle

--- project/src/unittests/RenderTests.java
+++ project/src/unittests/RenderTests.java
@@ -10,9 +10,9 @@ import renderer.Render;
 import scene.Scene;
 
 /**
- * Test rendering a basic image
+ * Test rendering abasic image
  * 
- * @author ayala and naama
+ * @author Dan
  */
 public class RenderTests {
 
@@ -23,10 +23,12 @@ public class RenderTests {
     @Test
     public void basicRenderTwoColorTest() {
         Scene scene = new Scene("Test scene");
-        scene.set_camera(new Camera(Point3D.ZERO,new Vector(0, 0, 1), new Vector(0, -1, 0)));
+        scene.set_camera(new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)));
         scene.set_distance(100);
-        scene.set_background(new Color(75,127,90));
+        scene.set_background(new Color(75, 127, 90));
         scene.set_ambientLight(new AmbientLight(new Color(255, 191, 191), 1));
+
+        scene.addGeometries(new Sphere(50, new Point3D(0, 0, 100)));
 
         scene.addGeometries(
                 new Triangle(new Point3D(100, 0, 100), new Point3D(0, 100, 100), new Point3D(100, 100, 100)),
@@ -34,14 +36,39 @@ public class RenderTests {
                 new Triangle(new Point3D(-100, 0, 100), new Point3D(0, 100, 100), new Point3D(-100, 100, 100)),
                 new Triangle(new Point3D(-100, 0, 100), new Point3D(0, -100, 100), new Point3D(-100, -100, 100)));
 
-        scene.addGeometries(new Sphere(50, new Point3D(0, 0, 100)));
-        
         ImageWriter imageWriter = new ImageWriter("base render test", 500, 500, 500, 500);
         Render render = new Render(imageWriter, scene);
 
-        render.renderImage();//our image
-        render.printGrid(50, java.awt.Color.YELLOW);//grid
-        render.writeToImage();//into files
+        render.renderImage();
+        render.printGrid(50, java.awt.Color.YELLOW);
+        render.writeToImage();
     }
+    
+    @Test
+    public void basicRenderMultiColorTest() {
+        Scene scene = new Scene("Test scene");
+        scene.set_camera(new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)));
+        scene.set_distance(100);
+        scene.set_background(Color.BLACK);
+        scene.set_ambientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2));
 
+        scene.addGeometries(new Sphere(50, new Point3D(0, 0, 100)));
+
+        scene.addGeometries(
+                new Triangle(new Color(java.awt.Color.BLUE),
+                        new Point3D(100, 0, 100), new Point3D(0, 100, 100), new Point3D(100, 100, 100)),      // lower right
+                new Triangle(
+                        new Point3D(100, 0, 100), new Point3D(0, -100, 100), new Point3D(100, -100, 100)),    // upper right
+                new Triangle(new Color(java.awt.Color.RED),
+                        new Point3D(-100, 0, 100), new Point3D(0, 100, 100), new Point3D(-100, 100, 100)),    // lower left
+                new Triangle(new Color(java.awt.Color.GREEN),
+                        new Point3D(-100, 0, 100), new Point3D(0, -100, 100), new Point3D(-100, -100, 100))); // upper left
+
+        ImageWriter imageWriter = new ImageWriter("color render test", 500, 500, 500, 500);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.printGrid(50, java.awt.Color.WHITE);
+        render.writeToImage();
+    }
 }
