451efb1a8592b124040f32a79da8ecb762d948fa
ayala <ayala@DESKTOP-FM0ES7E>
2020-06-15 21:22:28 +0300

    picture for step 7

--- project/src/unittests/ReflectionRefractionTests.java
+++ project/src/unittests/ReflectionRefractionTests.java
@@ -117,21 +117,59 @@ public class ReflectionRefractionTests
 		scene.set_ambientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
 
 		scene.addGeometries(
-				new Triangle(Color.BLACK, new Material(0.8, 0.2, 300, 0.6, 0),
-                        new Point3D(0, -50, 150), new Point3D(-50, 50, 150), new Point3D(50, 50, 50)),
-                new Triangle(Color.BLACK, new Material(0.8, 0.2, 300),
-                        new Point3D(-150, 150, 150), new Point3D(-70, 100, 50), new Point3D(75, -75, -150)),
-                new Triangle(Color.BLACK, new Material(0.8, 0.2, 300),
-                        new Point3D(75, -75, 150), new Point3D(-70, 100, 50), new Point3D(150, 0, 150)),
-                new Sphere(new Color(java.awt.Color.MAGENTA), new Material(0.2, 0.2, 30, 0, 0.6), 20, new Point3D(-30, 15, 50)),
-                new Sphere(new Color(java.awt.Color.red), new Material(0.2, 0.2, 30, 0.2, 0), 10, new Point3D(-10, 20, 10)));
+				//floor
+				new Triangle(new Color(java.awt.Color.BLACK), new Material(0.2, 0.2, 50, 0, 0),
+                        new Point3D(-150, 27, 150)/*top point*/, new Point3D(-100, 120, 0), new Point3D(100, 120, 0)),
+				new Triangle(new Color(java.awt.Color.BLACK), new Material(0.2, 0.2, 50, 0, 0),
+                        new Point3D(150, 27, 150), new Point3D(-150, 27, 150), new Point3D(100, 120, 0)),
+				//wall
+				new Triangle(new Color(89, 24, 94), new Material(0.2, 0.2, 50, 0.6, 0),
+                        new Point3D(-150, 27, 150), new Point3D(120, 27, 150), new Point3D(120, -120, 150)),
+				new Triangle(new Color(89, 24, 94), new Material(0.2, 0.2, 50, 0.6, 0),
+                      new Point3D(-150, 27, 150), new Point3D(-120, -120, 150), new Point3D(120, -120, 150)),
+				
+				//big ball
+                new Sphere(new Color(36,24,111), new Material(0.2, 0.4, 10, 0.9, 0), 67, new Point3D(0, 73, 100)),
+                
+                //orange ball
+                new Sphere(new Color(255, 100, 0), new Material(0.3, 0.3, 60, 0, 0), 7, new Point3D(-22, 38, 120)),
+                
+                //
+                //new Sphere(new Color(0,34,255), new Material(0.2, 0.2, 5, 0.9, 0), 12, new Point3D(0, 73, 100))
+				
+                
+                //picture on the wall
+                
+                //brown rectangle
+                new Triangle(new Color(119, 64, 56), new Material(0.2, 0.2, 50, 0, 0),
+                        new Point3D(-85, -80, 149)/*top point*/, new Point3D(-85, -35, 149), new Point3D(-50, -35, 149)),
+				new Triangle(new Color(119, 64, 56), new Material(0.2, 0.2, 50, 0, 0),
+                new Point3D(-85, -80, 149), new Point3D(-50,-80 , 149), new Point3D(-50, -35, 149))
+				
+				//white rictangle
+				
+                
+				);
 
 		scene.addLights(
-				new DirectionalLight(new Color(0, 50, 5), new Vector(1,0,1)),
+				new DirectionalLight(new Color(196, 255, 200), new Vector(1, 0.5, 1)),
+				//mini lights on the floor
+				new PointLight(new Color(248, 169, 153), new Point3D(-150, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(-108, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(-66, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(-24, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(18, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(60, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(102, 26, 149), 1, 0.0005, 0.0005),
+				new PointLight(new Color(248, 169, 153), new Point3D(144, 26, 149), 1, 0.0005, 0.0005)
+				
+				
+				//new SpotLight(new Color(248, 169, 153), new Point3D(90, -120, 150), new Vector(-1,1,0), 1, 0.00005, 0.0000005)
+				/*new DirectionalLight(new Color(java.awt.Color.WHITE), new Vector(-1,1,0)),
         		new PointLight(new Color(java.awt.Color.BLUE), new Point3D(-10, 20,10 ), 1.5, 0.0005, 0.00005),
-        		new SpotLight(new Color(java.awt.Color.CYAN), new Point3D(-75, 75, -50), new Vector(-1, -1, 1), 1, 0.00005, 0.0000005),
+        		,
         		new PointLight(new Color(java.awt.Color.MAGENTA), new Point3D(85, -70, 137), 1, 0.0000001, 0.00000001),
-        		new SpotLight(new Color(java.awt.Color.MAGENTA), new Point3D(60, 50, 0), new Vector(0, 1, 1), 1, 4E-5, 2E-7));
+        		new SpotLight(new Color(java.awt.Color.MAGENTA), new Point3D(60, 50, 0), new Vector(0, 1, 1), 1, 4E-5, 2E-7)*/);
 
 		ImageWriter imageWriter = new ImageWriter("picture", 200, 200, 600, 600);
 		Render render = new Render(imageWriter, scene);
