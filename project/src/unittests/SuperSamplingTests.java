224d2f983eeef29b60a30455d0cea92979cfa548
ayala <ayala@DESKTOP-FM0ES7E>
2020-06-22 01:35:35 +0300

    start heart

--- project/src/unittests/SuperSamplingTests.java
+++ project/src/unittests/SuperSamplingTests.java
@@ -21,18 +21,48 @@ public class SuperSamplingTests
 
 		scene.addGeometries(
 				//floor
-				new Triangle(new Color(java.awt.Color.BLACK), new Material(0.2, 0.2, 50, 0, 0.6),
-                        new Point3D(-150, 27, 150)/*top point*/, new Point3D(-100, 180, 0), new Point3D(100, 180, 0)),
-				new Triangle(new Color(java.awt.Color.BLACK), new Material(0.2, 0.2, 50, 0, 0.6),
-                        new Point3D(150, 27, 150), new Point3D(-150, 27, 150), new Point3D(100, 180, 0)),
+				new Triangle(new Color(java.awt.Color.WHITE), new Material(0.2, 0.2, 50, 0, 0),
+                        new Point3D(-150, 120, 100)/*top point*/, new Point3D(-100, 180, 0), new Point3D(100, 180, 0)),
+				new Triangle(new Color(java.awt.Color.WHITE), new Material(0.2, 0.2, 50, 0, 0),
+                        new Point3D(150, 120, 100), new Point3D(-150, 120, 100), new Point3D(100, 180, 0)),
 				//wall
-				new Triangle(Color.BLACK, new Material(0.2, 0.2, 50, 0.6, 0),
+				new Triangle(Color.BLACK, new Material(0.2, 0.2, 50, 0, 0),
                         new Point3D(-150, 27, 150), new Point3D(120, 27, 150), new Point3D(120, -120, 150)),
-				new Triangle(Color.BLACK, new Material(0.2, 0.2, 50, 0.6, 0),
-                      new Point3D(-150, 27, 150), new Point3D(-120, -120, 150), new Point3D(120, -120, 150))
+				new Triangle(Color.BLACK, new Material(0.2, 0.2, 50, 0, 0),
+                      new Point3D(-150, 27, 150), new Point3D(-120, -120, 150), new Point3D(120, -120, 150)),
+				
+				//heart
+				
+				//right side
+				new Triangle(new Color(java.awt.Color.RED), new Material(0.2, 0.2, 50, 0, 0),
+                     new Point3D(0, 0, 130), new Point3D(40, 0, 130), new Point3D(0, 40, 130)),
+				
+				new Triangle(new Color(java.awt.Color.PINK), new Material(0.2, 0.2, 50, 0, 0),
+	                     new Point3D(30, 30, 130), new Point3D(40, 0, 130), new Point3D(0, 40, 130)),
+				
+				new Triangle(new Color(java.awt.Color.MAGENTA), new Material(0.2, 0.2, 50, 0, 0),
+	                     new Point3D(30, 30, 130), new Point3D(0, 40, 130), new Point3D(2, 60, 130)),
+				
+				new Triangle(new Color(255, 4, 189), new Material(0.2, 0.2, 50, 0, 0),
+	                     new Point3D(2, 60, 130), new Point3D(30, 30, 130), new Point3D(30, 35, 130)),
+				
+				//light magenta
+				new Triangle(new Color(238, 99, 247), new Material(0.2, 0.2, 50, 0, 0),
+	                     new Point3D(30, 30, 130), new Point3D(48, 0, 130), new Point3D(30, 35, 130)),
+				
+				//burgundy
+				new Triangle(new Color(146, 20, 33), new Material(0.2, 0.2, 50, 0, 0),
+	                     new Point3D(40, 0, 130), new Point3D(48, 0, 130), new Point3D(30, 30, 130))
+				
+				//baby pink
+				//new Triangle(new Color(238, 132, 143), new Material(0.2, 0.2, 50, 0, 0),
+	              //       new Point3D(32, 0, 130), new Point3D(48, 0, 130), new Point3D(30, 30, 130))
+				
 				
 				);
 		scene.addLights(
+				
+				new DirectionalLight(new Color(java.awt.Color.pink), new Vector(0, 0.5, 1))
 				
 				
 				);
